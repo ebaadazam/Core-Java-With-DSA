bb38677e7a5143ed6e1bb3e96f3a477aa5262336
ebaadazam <ebaad.azam@memorly.ai>
2024-10-15 21:15:30 +0530

    reverseArray

---
+++ Arrays_ReverseArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class Arrays_ReverseArray {
+    public static void reverse(int[] nums) {
+        for (int i = 0; i < nums.length/2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length-1-i];
+            nums[nums.length-1-i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        reverse(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
+import java.util.Arrays;
+public class Arrays_ReverseArray {
+    public static void reverse(int[] nums) {
+        int i=0, j=nums.length-1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        reverse(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
--- CoreJavaPractice.java
+++ CoreJavaPractice.java
@@ -1,27 +1,33 @@
-// import java.util.ArrayList;
 // import java.util.Arrays;
-// import java.util.List;
-// import java.util.Scanner;
 // public class CoreJavaPractice {
-//     public static void mergeSort(int[] nums, int left, int right) {
-//         // base condition
-//         if (left >= right) { return; }
-//         int mid = (left+right)/2;
-//         mergeSort(nums, left, mid);
-//         mergeSort(nums, mid+1, right);
-//         merge(nums, left, mid, right); }
-//     public static void merge(int[] nums, int left, int mid, int right) {
-//         int i = left, j = mid+1;
-//         List<Integer> arr = new ArrayList<>();
-//         while (i <= mid && j <= right) {
-//             if (nums[i] <= nums[j]) { arr.add(nums[i]); i++; }
-//             else if (nums[j] < nums[i]) { arr.add(nums[j]); j++; } }
-//         while (i <= mid) { arr.add(nums[i]); i++; }
-//         while (j <= right) { arr.add(nums[j]); j++; }
-//         for (int k = 0; k < arr.size(); k++) {
-//             nums[left+k] = arr.get(k); } }
+//     public static void reverse(int[] nums) {
+//         for (int i = 0; i < nums.length/2; i++) {
+//             int temp = nums[i];
+//             nums[i] = nums[nums.length-1-i];
+//             nums[nums.length-1-i] = temp;
+//         }
+//     }
 //     public static void main(String[] args) {
-//         int[] nums = {5, 2, 7, 12, 28, 15};
-//         mergeSort(nums, 0, nums.length-1);
-//         for (Integer i : nums) { System.out.print(i + " "); } System.out.println(); } }
+//         int[] nums = {1, 2, 3, 4, 5, 6};
+//         reverse(nums);
+//         System.out.println(Arrays.toString(nums));
+//     }
+// }
 
+import java.util.Arrays;
+public class CoreJavaPractice {
+    public static void reverse(int[] nums) {
+        int i=0, j=nums.length-1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        reverse(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
